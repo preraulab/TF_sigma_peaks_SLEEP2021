3914a5ce546ca2ce5d7a9be1e95b2534030183a9
He, Mingjian <mhe6@mgh.harvard.edu>
2020-11-23 20:00:19 -0500

    update

--- example_data/example_script_use_TF_peak_detection.m
+++ example_data/example_script_use_TF_peak_detection.m
@@ -2,19 +2,15 @@
 %   This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
 %   (http://creativecommons.org/licenses/by-nc-sa/4.0/)
 %
-%   Authors: Mingjian He, Tanya Dimitrov, Michael Prerau
+%   Authors: Mingjian He, Michael Prerau
 %% ***********************************************************************
-close all
-clear all
-clc
+close all; clear all; clc
 
-% simply run
-load('example_data')
+% This data is taken from channel 3 from NSS11_2_200Hz_clin_unfilt.mat 
+load('example_data') % load the data 
 
-% run the TF_peak_detection wrapper function
-[ spindle_table, spectrogram_used, fpeak_proms, noise_peak_times, lowbw_TFpeaks, fh ]...
-    = TF_peak_detection(EEG, Fs, {stage_times, stages}, 'to_plot', false);
-
-
-
-
+% run the TF_peak_detection wrapper function to detect TF peaks during
+% NREM2 sleep, with TF peak central frequencies confined to 10-16Hz
+% Note: consider using clims.m to adjust color scale
+[ TFpeaks_table, spectrogram_used, fpeak_proms, fpeak_properties, tpeak_properties ] = TF_peak_detection(EEG, Fs, {stage_times, stages},...
+        'to_plot',true, 'spindle_freq_range',[10,16], 'detection_stages',[2]);
